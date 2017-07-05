3a8de1f19faa25bc793c83bb65cd0d09dd05aba6
zjpeng <zjpeng@Ctrip.com>
2017-07-05 20:35:53 +0800

    stash-add vuex

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <header-com></header-com>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    'header-com': Header
+  }
 }
 </script>
 
@@ -16,8 +20,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
