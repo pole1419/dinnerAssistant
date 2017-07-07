5c7230226826d579066f6aee5806183db1234b4d
zjpeng <zjpeng@Ctrip.com>
2017-07-07 18:10:25 +0800

    stash-static page stash

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,15 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+   
   },
-  //事件处理函数
-  bindViewTap: function() {
+  toPage: function(event) {
+    const page = event.target.dataset.page
+    const href = `../${page}/${page}`
     wx.navigateTo({
-      url: '../logs/logs'
+      url: href
     })
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
   }
 })
