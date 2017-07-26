15884c1e18d635592f8bffa383aeee5629359b35
zjpeng <zjpeng@Ctrip.com>
2017-07-26 19:22:44 +0800

    stash-add files

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<header className="header">
+				<i></i>			
+				<span>title</span>
+			</header>
+		)
+	}
+}
+
+export default Header
